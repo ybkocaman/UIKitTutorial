8e3946eab3c400cfc9f3e3d97eaace491d122c73
Yusuf <ybkocaman@gmail.com>
2024-05-11 16:29:06 +0100

    Initial Commit

---
+++ UIKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTutorial
+//
+//  Created by Yusuf Burak on 11/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
